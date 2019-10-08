9142ac3e45e5c9f4370cdf80139790c339e70860
Kuran V Kumar <kurankumar@gmail.com>
2019-10-08 20:43:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,6 +28,22 @@ class Person
     @job = job
   end
   
+  def name()
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job()
+    @job
+  end
+  
+  def job=(job)
+    @job
+  end
+  
   
 end #ends the Person class
 
